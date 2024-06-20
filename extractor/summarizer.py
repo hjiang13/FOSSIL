0ac22e7c7e429b2f516aff7bf32bea5686d4b8c5
hjiang13 <hjiang13@kent.edu>
2024-06-19 22:57:18 -0400

    Model changed to codeBERT

--- extractor/summarizer.py
+++ extractor/summarizer.py
@@ -1,8 +1,11 @@
-from transformers import pipeline
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-# Load the summarization pipeline
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+# Load the CodeBERT model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("microsoft/codebert-base")
 
-def summarize_code_with_huggingface(snippet):
-    summary = summarizer(snippet, max_length=10, min_length=5, do_sample=False)
-    return summary[0]['summary_text']
+def summarize_code_with_codebert(snippet):
+    inputs = tokenizer.encode("summarize: " + snippet, return_tensors="pt", max_length=512, truncation=True)
+    summary_ids = model.generate(inputs, max_length=10, min_length=3, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
