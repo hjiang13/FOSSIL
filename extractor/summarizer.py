efb7a4e10b358db644018449f0559ac796505986
hjiang13 <hjiang13@kent.edu>
2024-06-19 23:00:18 -0400

    debug

--- extractor/summarizer.py
+++ extractor/summarizer.py
@@ -1,11 +1,11 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-# Load the CodeBERT model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
-model = AutoModelForSeq2SeqLM.from_pretrained("microsoft/codebert-base")
+# Load the CodeT5 model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/codet5-base")
+model = AutoModelForSeq2SeqLM.from_pretrained("Salesforce/codet5-base")
 
-def summarize_code_with_codebert(snippet):
+def summarize_code_with_codet5(snippet):
     inputs = tokenizer.encode("summarize: " + snippet, return_tensors="pt", max_length=512, truncation=True)
-    summary_ids = model.generate(inputs, max_length=10, min_length=3, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary_ids = model.generate(inputs, max_length=50, min_length=10, length_penalty=2.0, num_beams=4, early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
