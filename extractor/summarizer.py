8b6305f0e10973aa6e1bab30a1aac26a92a86055
hjiang13 <hjiang13@kent.edu>
2024-06-21 15:24:06 -0400

    debug

--- extractor/summarizer.py
+++ extractor/summarizer.py
@@ -1,10 +1,13 @@
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+import transformers
+import torch
+from transformers import AutoTokenizer
 
 # Load the CodeLlama model and tokenizer
 
-access_token = "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"
+
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/CodeLlama-7b-hf")
-model = AutoModelForSeq2SeqLM.from_pretrained("meta-llama/CodeLlama-7b-hf")
+model = "meta-llama/CodeLlama-7b-hf"
 
 def summarize_code_with_codellama(snippet):
     inputs = tokenizer.encode("summarize: " + snippet, return_tensors="pt", max_length=512, truncation=True)
